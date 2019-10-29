f4152db7325a937177bb873e890e28b5d97d669c
Sascha Schmidt <sascha.schmidt91@gmx.de>
2019-10-29 19:54:05 +0100

    remove old data

--- include/parse_opts.h
+++
@@ -1,8 +0,0 @@
-#ifndef __PARSE_OPTS_H__
-#define __PARSE_OPTS_H__
-
-#include "common.h"
-
-void parse_opts(int argc, char *argv[]);
-
-#endif
--- include/state_handler.h
+++
@@ -1,8 +0,0 @@
-#ifndef __STATE_HANDLER_H__
-#define __STATE_HANDLER_H__
-
-#include "common.h"
-
-void state_machine_update(void);
-void state_handler_main(const char *topic);
-#endif
